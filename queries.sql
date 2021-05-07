2f49f860d79c3281139e45b1681a16caeb86b19c
Crystal Tong <45619087+xtaltong@users.noreply.github.com>
2021-05-06 17:41:30 -0700

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,19 @@
+SELECT P.pid
+FROM parts P, catalog C
+WHERE P.pid = C.pid AND C.cost < 10;
+
+SELECT P.pname //cost is in catalog
+FROM parts P, catalog C
+WHERE P.pid = C.pid AND C.cost < 10;
+
+SELECT S.address //join the three tables with common attributes
+FROM suppliers S, catalog C, parts P
+WHERE P.pname = "Fire Hydrant Cap" AND P.pid = C.pid AND C.sid = S.sid;
+
+SELECT S.sname
+FROM suppliers S, catalog C, parts P
+WHERE P.color = "green" AND P.pid = C.pid AND S.sid = C.sid;
+
+SELECT S.sname, P.pname
+FROM suppliers S, catalog C, parts P
+WHERE S.sid = C.sid AND P.pid = C.pid;
