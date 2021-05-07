7a92b0306710270064ef47491e5d0ca4963d7be2
Crystal Tong <45619087+xtaltong@users.noreply.github.com>
2021-05-06 17:45:42 -0700

    proper quotations

--- queries.sql
+++ queries.sql
@@ -8,11 +8,11 @@ WHERE P.pid = C.pid AND C.cost < 10;
 
 SELECT S.address
 FROM suppliers S, catalog C, parts P
-WHERE P.pname = "Fire Hydrant Cap" AND P.pid = C.pid AND C.sid = S.sid;
+WHERE P.pname = 'Fire Hydrant Cap' AND P.pid = C.pid AND C.sid = S.sid;
 
 SELECT S.sname
 FROM suppliers S, catalog C, parts P
-WHERE P.color = "green" AND P.pid = C.pid AND S.sid = C.sid;
+WHERE P.color = 'green' AND P.pid = C.pid AND S.sid = C.sid;
 
 SELECT S.sname, P.pname
 FROM suppliers S, catalog C, parts P
